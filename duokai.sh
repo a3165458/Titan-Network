d7034b4af5d63adc56a6cee3e087741d71c37674
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-16 00:02:52 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -54,8 +54,7 @@ do
     # 进入容器并执行绑定和其他命令
     docker exec -it $container_id bash -c "\
         apt update -y; \
-        apt install wget -y; \
-        wget https://letsencrypt.org/certs/isrgrootx1.der; \
+        apt install ca-certificates -y; \
         titan-edge bind --hash=$id https://api-test1.container1.titannet.io/api/v2/device/binding"
 done
 
