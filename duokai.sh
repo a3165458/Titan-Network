61876170c7cfac3b156940615395732c4790106b
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-15 18:38:39 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "脚本以及教程由推特用户大赌哥 @y95277777 编写，免费开源，请勿相信收费"
+echo "================================================================"
+echo "节点社区 Telegram 群组:https://t.me/niuwuriji"
+echo "节点社区 Telegram 频道:https://t.me/niuwuriji"
 
 # 读取加载身份码信息
 read -p "输入你的身份码: " id
