38956576aa6d66e54b6814fe5d39fae7dec7bdc4
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-25 17:45:27 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -36,7 +36,7 @@ else
 fi
 
 # 拉取Docker镜像
-docker pull nezha123/titan-edge
+docker pull nezha123/titan-edge:1.2
 
 
 # 创建用户指定数量的容器
@@ -47,7 +47,7 @@ do
     mkdir -p "$storage"
 
     # 运行容器，并设置重启策略为always
-    container_id=$(docker run -d --restart always -v "$PWD/$storage:/root/.titanedge/storage" --name "titan$i" nezha123/titan-edge)
+    container_id=$(docker run -d --restart always -v "$PWD/$storage:/root/.titanedge/storage" --name "titan$i" nezha123/titan-edge:1.2)
 
     echo "节点 titan$i 已经启动 容器ID $container_id"
 
