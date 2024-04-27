e9358b8920876d88809668ea7c75633888582460
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-27 21:43:32 +0800

    Update duokai.sh

--- duokai.sh
+++ duokai.sh
@@ -58,7 +58,7 @@ do
     mkdir -p "$storage_path"
 
     # 运行容器，并设置重启策略为always
-    container_id=$(docker run -d --restart always -v "$storage_path:/root/.titanedge/storage" --name "titan$i" nezha123/titan-edge:1.4)
+    container_id=$(docker run -d --restart always -v "$storage_path:/root/.titanedge/storage" --name "titan$i" --net=host  nezha123/titan-edge:1.4)
 
     echo "节点 titan$i 已经启动 容器ID $container_id"
 
